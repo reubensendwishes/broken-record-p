4bb6766b7c737b32bce4b6eb72b91daecacfda8c
Reuben <vcismygoal@gmail.com>
2026-04-19 06:28:10 +0800

    chore: migrate from @nuxtjs/google-fonts to @nuxt/fonts

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,7 @@
 <template>
-    <div class="bg-default text-primary">
-        <NuxtRouteAnnouncer />
-        <NuxtPage />
-    </div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
 </template>
-<script lang="ts" setup>
-    useHead({
-        link: [
-            {
-                rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,FILL@20..48,0..1&icon_names=add,arrow_back,arrow_left,arrow_right,close,content_copy,dark_mode,delete,drag_pan,edit,keyboard_arrow_down,keyboard_arrow_right,light_mode,menu,play_arrow,repeat,skip_next,skip_previous,volume_up',
-            },
-        ],
-    })
-</script>
+<script lang="ts" setup></script>
+
+<style scoped></style>
